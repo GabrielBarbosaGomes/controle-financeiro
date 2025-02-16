71533f8ffac124b122cd1797a06be80cfb0b6d87
Gabriel <gabryel122crf@gmail.com>
2025-02-16 16:32:16 -0300

    iniciando projeto

---
+++ app/pages/login/login.tsx
@@ -0,0 +1,25 @@
+import type { Route } from "../login/+types/login";
+import Input from '@mui/joy/Input';
+import logo from "../../assets/img/logo.png";
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Login() {
+  return(
+    <div className="flex items-center justify-center flex-col gap-5">
+      <img 
+        alt="logo"
+        src={logo}
+        className="max-w-[8em] max-h-[8em]" 
+      />
+      <div className="flex flex-col max-w-4xl gap-2">
+        <Input color="primary" placeholder="email" size="md"/>
+        <Input color="primary" placeholder="senha" size="md"/>
+      </div>
+    </div>
+  );
+}

3419d07c4bb17423ac3558ce5807fb0bf7efc67d
Gabriel <gabryel122crf@gmail.com>
2025-03-18 16:58:31 -0300

    esboço tela login

--- app/pages/login/login.tsx
+++ app/pages/login/login.tsx
@@ -1,6 +1,7 @@
 import type { Route } from "../login/+types/login";
-import Input from '@mui/joy/Input';
+import Input from "@mui/joy/Input";
 import logo from "../../assets/img/logo.png";
+import { Button } from "@mui/material";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
@@ -9,16 +10,15 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Login() {
-  return(
+  return (
     <div className="flex items-center justify-center flex-col gap-5">
-      <img 
-        alt="logo"
-        src={logo}
-        className="max-w-[8em] max-h-[8em]" 
-      />
+      <img alt="logo" src={logo} className="max-w-[8em] max-h-[8em]" />
       <div className="flex flex-col max-w-4xl gap-2">
-        <Input color="primary" placeholder="email" size="md"/>
-        <Input color="primary" placeholder="senha" size="md"/>
+        <Input color="primary" placeholder="email" size="md" />
+        <Input color="primary" placeholder="senha" size="md" />
+        <Button variant="contained">
+          Logar
+        </Button>
       </div>
     </div>
   );
